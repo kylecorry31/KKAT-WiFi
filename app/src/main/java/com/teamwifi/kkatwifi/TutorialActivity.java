4ea7273169e4c9a38b2e38777507db93990fab71
Kyle Corry <kylecorry31@gmail.com>
2016-04-02 13:20:25 -0700

    First commit

---
+++ app/src/main/java/com/teamwifi/kkatwifi/TutorialActivity.java
@@ -0,0 +1,29 @@
+package com.teamwifi.kkatwifi;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/teamwifi/kkatwifi/util/KKATWiFIHelper.java
@@ -0,0 +1,7 @@
+package com.teamwifi.kkatwifi.util;
+
+/**
+ * Created by Kylec on 4/2/2016.
+ */
+public class KKATWiFIHelper {
+}
