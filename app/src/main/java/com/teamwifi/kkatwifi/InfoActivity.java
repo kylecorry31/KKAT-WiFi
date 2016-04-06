2d8e02fba231e5660f3ee47e707a4ef245203454
Kyle Corry <kylecorry31@gmail.com>
2016-04-06 14:08:38 -0400

    Complete InfoActivity

--- app/src/main/java/com/teamwifi/kkatwifi/InfoActivity.java
+++ app/src/main/java/com/teamwifi/kkatwifi/InfoActivity.java
@@ -1,13 +1,26 @@
 package com.teamwifi.kkatwifi;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.teamwifi.kkatwifi.util.KKATWiFiHelper;
 
 public class InfoActivity extends AppCompatActivity {
+
+    TextView ssidText, percentText, powerText, frequencyText;
+    KKATWiFiHelper wiFiHelper;
+    private BroadcastReceiver mRSSIBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +29,37 @@ public class InfoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        wiFiHelper = new KKATWiFiHelper(this);
+
+        ssidText = (TextView) findViewById(R.id.ssid);
+
+        percentText = (TextView) findViewById(R.id.percent);
+
+        powerText = (TextView) findViewById(R.id.power);
+
+        frequencyText = (TextView) findViewById(R.id.frequency);
+
+        mRSSIBroadcastReceiver = new BroadcastReceiver() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onReceive(Context context, Intent intent) {
+                ssidText.setText(wiFiHelper.getSSID());
+                percentText.setText(String.valueOf(wiFiHelper.getSignalStrength())+"%");
+                powerText.setText(String.valueOf(wiFiHelper.getRSSI())+" dBm");
+                frequencyText.setText(String.valueOf(wiFiHelper.getFrequency())+" MHz");
             }
-        });
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mRSSIBroadcastReceiver, new IntentFilter(WifiManager.RSSI_CHANGED_ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(mRSSIBroadcastReceiver);
+        super.onPause();
     }
 
 }
