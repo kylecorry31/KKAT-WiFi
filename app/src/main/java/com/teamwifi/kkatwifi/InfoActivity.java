9e6cd9c82c86d30a36cf09de3b19d616047c701f
Kyle Corry <kylecorry31@gmail.com>
2016-04-09 12:36:38 -0400

    Add deadzone indicator to InfoActivity

--- app/src/main/java/com/teamwifi/kkatwifi/InfoActivity.java
+++ app/src/main/java/com/teamwifi/kkatwifi/InfoActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.teamwifi.kkatwifi.util.KKATWiFiHelper;
@@ -21,6 +23,7 @@ public class InfoActivity extends AppCompatActivity {
     TextView ssidText, percentText, powerText, frequencyText;
     KKATWiFiHelper wiFiHelper;
     private BroadcastReceiver mRSSIBroadcastReceiver;
+    private FrameLayout indicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +42,20 @@ public class InfoActivity extends AppCompatActivity {
 
         frequencyText = (TextView) findViewById(R.id.frequency);
 
+        indicator = (FrameLayout) findViewById(R.id.indicator);
+
         mRSSIBroadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
+                int signalStrength = wiFiHelper.getSignalStrength();
                 ssidText.setText(wiFiHelper.getSSID());
-                percentText.setText(String.valueOf(wiFiHelper.getSignalStrength()) + "%");
+                percentText.setText(String.valueOf(signalStrength) + "%");
                 powerText.setText(String.valueOf(wiFiHelper.getRSSI()) + " dBm");
                 frequencyText.setText(String.valueOf(wiFiHelper.getFrequency()) + " MHz");
+                if (signalStrength >= 75)
+                    indicator.setBackgroundColor(Color.rgb(245 - (signalStrength - 75) * 9, (int) (241 + (signalStrength - 75) * 0.16), 12));
+                else
+                    indicator.setBackgroundColor(Color.rgb((int) (219 + (signalStrength * 0.35)), (int) (59 + signalStrength * 2.43), (int) (59 - (signalStrength * 0.63))));
             }
         };
     }
