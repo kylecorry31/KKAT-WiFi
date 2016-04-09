a633c9224a02f55086d333e1c26b40dd274da49b
Kyle Corry <kylecorry31@gmail.com>
2016-04-09 13:46:00 -0400

    Remove Toolbar and FAB from ResultActivity

--- app/src/main/java/com/teamwifi/kkatwifi/ResultActivity.java
+++ app/src/main/java/com/teamwifi/kkatwifi/ResultActivity.java
@@ -12,18 +12,8 @@ public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.layout_result);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
