cfad36da8859afc21f91a454e6d0e8c596f9dd47
Kyle Corry <kylecorry31@gmail.com>
2016-04-09 13:45:02 -0400

    Remove Toolbar from LearnActivity

--- app/src/main/java/com/teamwifi/kkatwifi/LearnActivity.java
+++ app/src/main/java/com/teamwifi/kkatwifi/LearnActivity.java
@@ -1,20 +1,14 @@
 package com.teamwifi.kkatwifi;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class LearnActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_learn);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.layout_learn);
 
     }
 
