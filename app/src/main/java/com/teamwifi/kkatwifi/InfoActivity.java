ee11f82ae37503a122fa5282614f5ca940839b4d
Kyle Corry <kylecorry31@gmail.com>
2016-04-09 13:44:49 -0400

    Remove Toolbar and add channel to InfoActivity

--- app/src/main/java/com/teamwifi/kkatwifi/InfoActivity.java
+++ app/src/main/java/com/teamwifi/kkatwifi/InfoActivity.java
@@ -7,20 +7,17 @@ import android.content.IntentFilter;
 import android.graphics.Color;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.teamwifi.kkatwifi.util.KKATWiFiHelper;
 
+import org.w3c.dom.Text;
+
 public class InfoActivity extends AppCompatActivity {
 
-    TextView ssidText, percentText, powerText, frequencyText;
+    TextView ssidText, percentText, powerText, frequencyText, channelText;
     KKATWiFiHelper wiFiHelper;
     private BroadcastReceiver mRSSIBroadcastReceiver;
     private FrameLayout indicator;
@@ -28,9 +25,7 @@ public class InfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.layout_info);
 
         wiFiHelper = new KKATWiFiHelper(this);
 
@@ -42,6 +37,8 @@ public class InfoActivity extends AppCompatActivity {
 
         frequencyText = (TextView) findViewById(R.id.frequency);
 
+        channelText = (TextView) findViewById(R.id.channel);
+
         indicator = (FrameLayout) findViewById(R.id.indicator);
 
         mRSSIBroadcastReceiver = new BroadcastReceiver() {
@@ -52,6 +49,7 @@ public class InfoActivity extends AppCompatActivity {
                 percentText.setText(String.valueOf(signalStrength) + "%");
                 powerText.setText(String.valueOf(wiFiHelper.getRSSI()) + " dBm");
                 frequencyText.setText(String.valueOf(wiFiHelper.getFrequency()) + " MHz");
+                channelText.setText("Channel " + String.valueOf(wiFiHelper.getChannel()));
                 if (signalStrength >= 75)
                     indicator.setBackgroundColor(Color.rgb(245 - (signalStrength - 75) * 9, (int) (241 + (signalStrength - 75) * 0.16), 12));
                 else
