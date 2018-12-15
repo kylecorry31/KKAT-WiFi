8179054ec3d95f7d14c81318d40a0daeffe3222a
Kyle Corry <kylecorry31@gmail.com>
2018-12-15 09:19:17 -0500

    Add bottom navigation

--- app/src/main/java/com/teamwifi/kkatwifibeta/MainActivity.kt
+++ app/src/main/java/com/teamwifi/kkatwifibeta/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.teamwifi.kkatwifibeta
 
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -17,12 +18,13 @@ import com.teamwifi.kkatwifibeta.util.WiFiNetwork
 import kotlinx.android.synthetic.main.layout_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     lateinit var currentNetwork: WiFiNetwork
     private lateinit var connectivityChangedReceiver: BroadcastReceiver
     lateinit var noConnectionBar: Snackbar
     
     override fun onCreate(savedInstanceState: Bundle?) {
+        // TODO: Request location permission
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main)
         window.setBackgroundDrawable(null)
@@ -31,9 +33,9 @@ class MainActivity : AppCompatActivity() {
         }
         currentNetwork = WiFiNetwork(this)
         noConnectionBar = Snackbar.make(coordinator, "Not connected to a WiFi network", Snackbar.LENGTH_INDEFINITE)
-        scanFab.setOnClickListener {
-            startActivity(Intent(applicationContext, BaseReadingActivity::class.java))
-        }
+//        scanFab.setOnClickListener {
+//            startActivity(Intent(applicationContext, BaseReadingActivity::class.java))
+//        }
 
         connectivityChangedReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
@@ -48,20 +50,21 @@ class MainActivity : AppCompatActivity() {
                 ssid.text = currentNetwork.ssid
                 val signalStrength = currentNetwork.signalStrength
                 strength.text = signalStrength.toString()
-                rssi.text = "${currentNetwork.rssi} dBm"
-                freq.text = "${currentNetwork.frequency / 100 / 10.0} GHz"
-                channel.text = "${currentNetwork.channel}"
+                rssi2.text = currentNetwork.rssi.toString()
+                freq2.text = (currentNetwork.frequency / 100 / 10.0).toString()
+                channel2.text = currentNetwork.channel.toString()
                 if (signalStrength <= 20)
                     desc.text = "Deadzone detected"
                 else if (signalStrength <= 50)
                     desc.text = "Weak signal detected"
                 else
-                    desc.text = "Signal is fine"
+                    desc.text = "Signal is strong"
                 val color: Int
-                if (signalStrength >= 75)
-                    color = Color.rgb(245 - (signalStrength - 75) * 9, (241 + (signalStrength - 75) * 0.16).toInt(), 12)
-                else
-                    color = Color.rgb((219 + signalStrength * 0.35).toInt(), (59 + signalStrength * 2.43).toInt(), (59 - signalStrength * 0.63).toInt())
+//                if (signalStrength >= 75)
+//                    color = Color.rgb(245 - (signalStrength - 75) * 9, (241 + (signalStrength - 75) * 0.16).toInt(), 12)
+//                else
+//                    color = Color.rgb((219 + signalStrength * 0.35).toInt(), (59 + signalStrength * 2.43).toInt(), (59 - signalStrength * 0.63).toInt())
+                color = Color.WHITE;
                 main.setBackgroundColor(color)
             }
         }
